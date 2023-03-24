64af54e9d0e916ab77b79a27d458a59243f93bed
+ <+>
2023-03-24 09:36:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+let url = 'https://anapioficeandfire.com/api/books'
+return fetch(url).then(function(response){
+  return response.json();
+})
+.then(function(data){
+  renderBooks(data);
+});
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
